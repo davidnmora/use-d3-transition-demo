77a2796e6b10a738995d2ec31454871ab30059b3
davidnmora <davidnmora@gmail.com>
2020-03-28 12:27:00 -0700

    Get full demo working (css, debugged npm package)

--- src/App.js
+++ src/App.js
@@ -2,24 +2,41 @@ import React, {useState, useCallback} from 'react';
 import './App.css';
 import TransitionableCircle from "./TransitionableCircle";
 
+
 function App() {
-  const [circlePosition, setCirclePosition] = useState({cx: 100, cy: 100})
+  const svgCanvas = {
+    width: window.innerWidth * 0.8,
+    height: 200,
+    margin: 20,
+  }
+  const left = {
+    cx: svgCanvas.margin,
+    cy: svgCanvas.height / 2,
+  }
+  const right = {
+    cx: svgCanvas.width - svgCanvas.margin,
+    cy: svgCanvas.height / 2,
+  }
+  
+  const [circlePosition, setCirclePosition] = useState(left)
   const togglePosition = useCallback(() => {
-    circlePosition.cx === 100
-      ? setCirclePosition({cx: 200, cy: 200})
-      : setCirclePosition({cx: 100, cy: 100})
+    circlePosition.cx === left.cx
+      ? setCirclePosition(right)
+      : setCirclePosition(left)
   }, [circlePosition])
   
   return (
     <div className="App">
       <h1><code>useD3Transition</code> Demo: animated circle</h1>
-      <button onClick={togglePosition}>Toggle circle position</button>
-      <svg>
+      <div>
+        <button onClick={togglePosition}>Toggle circle position</button>
+      </div>
+      <svg width={svgCanvas.width} height={svgCanvas.height} style={{backgroundColor: 'white'}}>
         <TransitionableCircle
           cx={circlePosition.cx}
           cy={circlePosition.cy}
           
-          r={50}
+          r={20}
           style={{fill: 'skyblue'}}
         />
       </svg>
--- src/TransitionableCircle.js
+++ src/TransitionableCircle.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import useD3Transition from 'use-d3-transition'
-// import useD3Transition from './useD3Transition'
+
 
 const TransitionableCircle = ({cx, cy, ...staticAttributes}) => {
 	const {ref, attrState} = useD3Transition({attrsToTransitionTo: {cx, cy}, deps: [cx, cy]})
