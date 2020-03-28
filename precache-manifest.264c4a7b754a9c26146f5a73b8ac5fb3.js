3fccdf183a951b1407660544ea9ca7d7bf5b64b7
davidnmora <davidnmora@gmail.com>
2020-03-28 13:51:20 -0700

    Updates

---
+++ precache-manifest.264c4a7b754a9c26146f5a73b8ac5fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf2334312bc7b433cecd122be72faf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44970acc2534bf26c6ce",
+    "url": "/static/css/main.10f181bf.chunk.css"
+  },
+  {
+    "revision": "1a637734c4d0277abe2c",
+    "url": "/static/js/2.dfdc0c3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfdc0c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44970acc2534bf26c6ce",
+    "url": "/static/js/main.1a9127a1.chunk.js"
+  },
+  {
+    "revision": "f440e20b4cf1fa7be9a9",
+    "url": "/static/js/runtime-main.2c84d587.js"
+  }
+]);
